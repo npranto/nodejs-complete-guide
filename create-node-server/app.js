2a8fa21939bb562889afa59497a2715b830e9a7f
Nazmuz Pranto <npranto@gmail.com>
2022-10-24 21:40:14 -0400

    accessing headers, http methods and sending response back to the client

--- create-node-server/app.js
+++ create-node-server/app.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('Welcome to your server!');
+  // identify route, HTTP method for a request
+  const { url, method } = req;
+  // setting a response header
+  res.setHeader('Content-Type', 'text/html');
+  // write response 
+  res.write('Hello World!')
+  // ending a response
+  res.end();
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+
+  console.log('Listening to port 3000');
+});
